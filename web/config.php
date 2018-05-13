b0dbd76ba7667cafdde69f74818c3078908d90b8
Li Chengxun <24319042+bclswl0827@users.noreply.github.com>
2018-05-13 13:16:43 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', 'AIzaSyCvAIxGp-hY0xLnYQ8vck_z0ZpayeODR5Y');
+define('APIKEY', 'AIzaSyARHJJDFtwb5wcnURwwJA3FQRrM16PF27c');
 define('GJ_CODE', 'JP');
 define('SITE_NAME', 'YouTube');
 define('TITLENAME', 'YouTube Mirror');
-define('EN2DEKEY', 'lichenjygjdjhdjgdjthfshtshshtakhgo664654624679576546246451&#12354;ngxjyfjydjgsjjydhthtsyrqyrhrskihllkujydjtagrhtshrakfjjydjgxun0827');
+define('EN2DEKEY', 'licdhyyeshenjygjdjhdjgdjthfshtshshtakhgo664654624679576546246451&#12354;ngxjyfjydjgsjjydhthtsyrqyrhrskihllkujydjtagrhtshrakfjjydjgxun');
 define('EMAIL', 'fankui_bbs@yahoo.co.jp');
 ?>
